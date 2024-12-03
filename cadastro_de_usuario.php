f384b9c3a7f7720737fe96c69e186d1093ee3019
VitorTorre <142807958+VitorTorre@users.noreply.github.com>
2024-12-03 18:26:32 -0300

    Add files via upload

--- cadastro_de_usuario.php
+++ cadastro_de_usuario.php
@@ -1,33 +1,6 @@
-
-
 <?php
     
-    // Código de acesso que faz o login na Dybit com a conta do usuário
- session_start();
- require_once 'MySQL/conexao.php';
- 
- // Verifica se o formulário foi submetido
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $email = $_POST["email"];
-   $senha = $_POST["senha"];
- 
-     // Consulta ao banco de dados
-   $query = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-   $result = $conn->query($query);
- 
-   if ($result->num_rows > 0) {
-     $_SESSION["usuario"] = $email;
-     header("Location: count-user.php");
-     exit;
-   } else {
-     echo "Login ou senha incorretos!";
-   }
-  } else {
-    // Se não foi submetido, exibe formulário
-    include "form-login-Dybit.php";
-  }
-
-    include "MySql/conexao.php";
+    include "conexao.php";
         $id = $_POST['id'];
         $nome = $_POST['nome'];
         $email = $_POST['email'];
@@ -38,18 +11,6 @@
         VALUES ('$nome', '$email', '$senha', '$confirmar_senha')";
        
     
-      header('Location: count-user.php');
-
-
+      header('Location: TCC Definitivo(index).html');
 
 ?>
-<form id="form-login">
-    <label>Usuário:</label>
-    <input type="text" id="usuario" name="usuario"><br><br>
-    <label>Senha:</label>
-    <input type="password" id="senha" name="senha"><br><br>
-    <button id="btn-login">Entrar</button>
-  </form>
-  <div id="mensagem"></div>
-
-  <script src="script.js"></script>
